254108af9e19d76382eca28f2363defb3ccbe45f
SNOW\Ether <etherealgarden@msn.com>
2026-05-17 02:28:18 +0900

    入力パスを変更

--- Assets/Uft.AssetBundleTools/Samples~/AssetBundleSample/AssetBundleSample.cs
+++ Assets/Uft.AssetBundleTools/Samples~/AssetBundleSample/AssetBundleSample.cs
@@ -11,20 +11,24 @@ namespace Uft.AssetBundleTools.Samples
         static AssetBundleHelper? helper;
 
         [SerializeField] Canvas? _canvas;
+        [SerializeField] string _assetPathBase = "Assets/Samples/AssetBundleSample/AssetBundle/";
+        [SerializeField] string _loadingRootPath = "Assets/StreamingAssets/AssetBundles/";
 
         void Start()
         {
             helper ??= new AssetBundleHelper(
-                    logInfo: (message) => Debug.Log(message),
-                    logError: (message) => Debug.LogError(message));
+                assetPathBase: this._assetPathBase,
+                loadingRootPath: this._loadingRootPath,
+                logInfo: (message) => Debug.Log(message),
+                logError: (message) => Debug.LogError(message));
 
-            var prefabA = helper.LoadFromAssetBundle<GameObject>("Assets/AssetBundle/AssetBundleSample/prefabA.prefab");
+            var prefabA = helper.LoadFromAssetBundle<GameObject>($"{this._assetPathBase}AssetBundleSample/prefabA.prefab");
             var instanceA = Instantiate(prefabA);
             var a = instanceA!.GetComponent<RectTransform>();
             a.SetParent(this._canvas!.transform, false);
             a.anchoredPosition = new Vector2(-100, 0);
 
-            var prefabB = helper.LoadFromAssetBundle<GameObject>("Assetbundlesample/prefabB");
+            var prefabB = helper.LoadFromAssetBundle<GameObject>("AssetBundleSample/prefabb");
             var instanceB = Instantiate(prefabB);
             var b = instanceB!.GetComponent<RectTransform>();
             b.SetParent(this._canvas.transform, false);
@@ -35,7 +39,7 @@ namespace Uft.AssetBundleTools.Samples
             UniTask.Void(async () =>
             {
                 await UniTask.Delay(1000, cancellationToken: ct);
-                var prefabC = await helper.LoadFromAssetBundleAsync<GameObject>("assetbundlesample/prefabc", ct);
+                var prefabC = await helper.LoadFromAssetBundleAsync<GameObject>($"{this._assetPathBase}AssetBundleSample/prefabC.prefab", ct);
                 var instanceC = Instantiate(prefabC);
                 var c = instanceC!.GetComponent<RectTransform>();
                 c.SetParent(this._canvas.transform, false);
@@ -45,7 +49,7 @@ namespace Uft.AssetBundleTools.Samples
             UniTask.Void(async () =>
             {
                 await UniTask.Delay(2000, cancellationToken: ct);
-                var prefabD = await helper.LoadFromAssetBundleAsync<GameObject>("Assets/AssetBundle/AssetBundleSample/prefabD.prefab", ct);
+                var prefabD = await helper.LoadFromAssetBundleAsync<GameObject>("AssetBundleSample/prefabd", ct);
                 var instanceD = Instantiate(prefabD);
                 var d = instanceD!.GetComponent<RectTransform>();
                 d.SetParent(this._canvas.transform, false);
